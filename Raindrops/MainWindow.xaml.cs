53ecfbeb87a596267e598a56e102e83b3b5430d4
robbe.gillis <robbe.gillis@B209-11>
2017-10-24 09:11:45 +0200

    e

--- Raindrops/MainWindow.xaml.cs
+++ Raindrops/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace Raindrops
 {
@@ -20,9 +22,62 @@ namespace Raindrops
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random randomNumber = new Random();
+        private double x, y, size;
+        private SolidColorBrush brush;
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            mijnCanvas.Children.Clear();
+        }
+
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        private void gapSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int timeGap = Convert.ToInt32(gapSlider.Value);
+            gapLabel.Content = Convert.ToString(timeGap);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gapLabel.Content = Convert.ToString(gapSlider.Value);
+            brush = new SolidColorBrush(Colors.DarkRed);
+            timer.Interval = TimeSpan.FromMilliseconds(gapSlider.Value);
+            timer.Tick += timer_Tick;
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            x = randomNumber.Next(0, Convert.ToInt32(mijnCanvas.Width));
+            y = randomNumber.Next(0, Convert.ToInt32(mijnCanvas.Height));
+            size = randomNumber.Next(1, 40);
+
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = size;
+            ellipse.Height = size;
+            ellipse.Stroke = brush;
+            ellipse.Fill = brush;
+            ellipse.Margin = new Thickness(x, y, 0, 0);
+            mijnCanvas.Children.Add(ellipse);
+
+            timer.Stop();
+            int ms = randomNumber.Next(1, Convert.ToInt32(gapSlider.Value));
+            timer.Interval = TimeSpan.FromMilliseconds(ms);
+            timer.Start();
         }
     }
 }
